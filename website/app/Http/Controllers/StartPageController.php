b9ff06547a69cc40e84d81b99b6a7b427ecd1a5e
aebeloe <m.aebeloe@gmail.com>
2026-01-24 22:36:52 +0100

    added footer

--- website/app/Http/Controllers/StartPageController.php
+++ website/app/Http/Controllers/StartPageController.php
@@ -10,12 +10,21 @@ class StartPageController
 {
     public function get()
     {
-
-
-
-
         return Inertia::render('Welcome', [
-            'canRegister' => false,
+            'menuData' => [
+                'Udvalg' => [
+                    ['label' => 'Fodbold', 'url' => '/udvalg/fodbold'],
+                    ['label' => 'Håndbold', 'url' => '/udvalg/haandbold'],
+                    ['label' => 'Gymnastik', 'url' => '/udvalg/gymnastik'],
+                    ['label' => 'Badminton', 'url' => '/udvalg/badminton'],
+                ],
+                'Om Foreningen' => [
+                    ['label' => 'Vedtægter', 'url' => '/om/vedtaegter'],
+                    ['label' => 'Bestyrelsen', 'url' => '/om/bestyrelsen'],
+                    ['label' => 'Privatlivspolitik', 'url' => '/om/privatliv'],
+                ],
+                // Add other keys as needed...
+            ]
         ]);
     }
 }
